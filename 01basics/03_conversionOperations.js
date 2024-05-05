b6b862228916ae01eb352e702335244c37b355d5
devraj894 <118315477+devraj894@users.noreply.github.com>
2024-05-05 15:55:51 +0000

    Conversion of datatypes

---
+++ 01basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score ="44abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber)
+
+// "22" => 33
+// "23abc" => NaN
+// true => 1
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "chintu" => true
+
+let someNumber = "3"
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
