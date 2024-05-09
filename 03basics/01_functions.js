a323e9a52fa12266c1f1883a20793816fc6060ee
devraj894 <118315477+devraj894@users.noreply.github.com>
2024-05-09 10:59:53 +0000

    Functions and Mini Hoisting done

--- 03basics/01_functions.js
+++ 03basics/01_functions.js
@@ -39,3 +39,45 @@ function userLogin1(username = "Bob"){ // if argument did not give then it will 
 
 console.log(userLogin1("alice"))
 
+function calculateCartPrice(...nums){ // it called as Rest/Spread Operator to handle multiple aguments and return in array formate.
+    // calculateCartPrice(val1, val2, ...nums) // Now in this case va1, val2 took both starting values and rest stored in array formate.
+    return nums
+}
+
+console.log(calculateCartPrice(100,200,300))
+
+// Object in Function......
+
+const user = {
+    username: "devraj",
+    price: 3000
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+//handleObject(user)
+
+handleObject({
+    username: "devraj",
+    price: 199
+})
+
+// Array in Function........
+
+const myNewArray = [100,200]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([100,20]))
+
+// Mini Hoisting..........
+addOne(2)
+const addOne = function(num){ // here addOne is known as expression and we can define functions also in this way...
+    // NOTE:- But in this way we can't access first before declaration.
+    return num+1
+}
